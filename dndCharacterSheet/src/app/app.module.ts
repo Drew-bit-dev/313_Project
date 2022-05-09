edc99dbc6b8510424dbd4ac4d0d2fd9bc732d833
Mindarius <67028450+Mindarius@users.noreply.github.com>
2022-05-09 16:40:59 -0500

    Fixed drew's botched merge (hopefully)
    imported header to app.module
    
    Also cleaned out app.module being used as a test environment.

--- dndCharacterSheet/src/app/app.module.ts
+++ dndCharacterSheet/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListboxComponent } from './listbox/listbox.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListboxComponent
+    ListboxComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
