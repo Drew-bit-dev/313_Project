3308d8fb1fe6d617ab126d62c7c079cff04c2d11
Mindarius <67028450+Mindarius@users.noreply.github.com>
2022-05-09 11:34:16 -0500

    Test environment
    test environment and minor fix to listbox .html

--- dndCharacterSheet/src/app/app.component.ts
+++ dndCharacterSheet/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Item } from './Item';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dndCharacterSheet';
+  list: Item[] = [
+    {
+      listText:'This is an item',
+      pointer:'This points to something',
+      tags:[]
+    },
+    {
+      listText:'This is a second item',
+      pointer:'This points to something else',
+      tags:[]
+    }
+  ];
 }
