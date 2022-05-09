3a249f2e3300d049a4b03678244ad5f677cabb0e
Mindarius <67028450+Mindarius@users.noreply.github.com>
2022-05-09 11:48:07 -0500

    popup with test
    popup component with test environment
    
    quite bad
    
    covers everything which works, but doesn't actually have the popup yet
    
    benching until needed

--- dndCharacterSheet/src/app/app.module.ts
+++ dndCharacterSheet/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListboxComponent } from './listbox/listbox.component';
+import { PopupComponent } from './popup/popup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListboxComponent
+    ListboxComponent,
+    PopupComponent
   ],
   imports: [
     BrowserModule,
