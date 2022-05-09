eaf9d697081a06a1a9e5c8ec4ad03fbdf2b7c594
Mindarius <67028450+Mindarius@users.noreply.github.com>
2022-05-09 16:24:26 -0500

    Selection
    made the player creation page
    
    It's ugly, but it works.
    
    Players can now select their class, race, background, and character name from a list of defaults (or textfield for name)
    
    Class/race/background details not currently displayed.
    
    Listbox has been changed to emit the selected item when the selected item is changed, also accepts a default argument.
    
    imported FormsModule to app.module for [(ngModel)] for input 2-way binding (character name)

--- dndCharacterSheet/src/app/app.module.ts
+++ dndCharacterSheet/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListboxComponent } from './listbox/listbox.component';
+import { PlayerCreationComponent } from './player-creation/player-creation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListboxComponent
+    ListboxComponent,
+    PlayerCreationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ dndCharacterSheet/src/app/player-creation/player-creation.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Item } from '../Item';
+
+@Component({
+  selector: 'app-player-creation',
+  templateUrl: './player-creation.component.html',
+  styleUrls: ['./player-creation.component.css']
+})
+export class PlayerCreationComponent implements OnInit {
+  selectedTab: string = "Class";
+
+  
+  
+  //TODO poke database for following lists
+  Classes: Item[] = [
+    {
+      listText:"Rogue",
+      pointer:"Look at rogue!",
+      tags:[]
+    }
+  ]
+  Races: Item[] = [
+    {
+      listText:"Halfling",
+      pointer:"Look at the short guy!",
+      tags:[]
+    }
+  ]
+  Backgrounds: Item[] = [
+    {
+      listText:"Criminal",
+      pointer:"I'm chaotic neutral guys, I swear!",
+      tags:[]
+    }
+  ]
+
+
+
+  selectedClass?: Item|null;
+  selectedRace?: Item|null;
+  selectedBackground?: Item|null;
+  @Input() name?: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  selectClass(i: Item|null){ this.selectedClass=i; }
+  selectBackground(i: Item|null){ this.selectedBackground=i; }
+  selectRace(i: Item|null){ this.selectedRace=i; }
+  selectTab(tab: string){
+    this.selectedTab=tab;
+  }
+  makeChar(){
+    //TODO
+  }
+
+}
