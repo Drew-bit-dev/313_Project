1b32f113fb24d6cb1fa4e13ecdf68034a1c44156
CLJMCLJM <103469963+CLJMCLJM@users.noreply.github.com>
2022-05-11 23:47:40 -0500

    speed component linked
    speed component linked with stats service

--- dndCharacterSheet/src/app/stats-page/speed/speed.component.ts
+++ dndCharacterSheet/src/app/stats-page/speed/speed.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { STATS } from '../Mock-Stats';
+import { StatsService } from 'src/app/stats.service';
+import * as types from 'src/app/types';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-speed',
@@ -9,8 +12,24 @@ import { STATS } from '../Mock-Stats';
 export class SpeedComponent implements OnInit {
 
   constructor() { }
-  stats = STATS;
+  stats = new StatsService();
+  playerStats: types.BaseStats = {
+    proficiencyBonus: 2,
+    maxHPBeforeCon: 0,
+    speed: 0,
+    hitDiceD6: 0,
+    hitDiceD8: 0,
+    hitDiceD10: 0,
+    hitDiceD12: 0,
+    strength: 10,
+    dexterity: 10,
+    constitution: 10,
+    intelligence: 10,
+    wisdom: 10,
+    charisma: 10,
+  };
   ngOnInit(): void {
+    this.stats.getBaseStats().subscribe(playerStats => this.playerStats = playerStats);
   }
 
 }
