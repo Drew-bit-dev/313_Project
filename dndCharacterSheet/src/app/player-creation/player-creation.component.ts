fbfe10be7ff03785f4857a709f424a8d41b24dd0
Mindarius <67028450+Mindarius@users.noreply.github.com>
2022-05-12 08:54:28 -0500

    consolidated checks if character ready for creation to 1 variable
    this way when we need to add in proficiency checks and such it won't be hideous

--- dndCharacterSheet/src/app/player-creation/player-creation.component.ts
+++ dndCharacterSheet/src/app/player-creation/player-creation.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Item } from '../Item';
+import { StatsService } from '../stats.service';
+import * as types from '../types';
 
 @Component({
   selector: 'app-player-creation',
@@ -11,7 +13,7 @@ export class PlayerCreationComponent implements OnInit {
   @Output() create = new EventEmitter<string>();
   align?:string;
 
-  setAlign(str:string){this.align=str}
+  setAlign(str:string){this.align=str;this.checkReady();}
 
   
   
@@ -44,19 +46,38 @@ export class PlayerCreationComponent implements OnInit {
   selectedRace?: Item|null;
   selectedBackground?: Item|null;
   @Input() name?: string;
+  readyToMake:boolean=false;
+
+  checkReady(){
+    if(this.selectedClass && this.selectedRace && this.selectedBackground){
+      console.log("selected")
+      this.readyToMake = true;
+    }
+    if(this.align=="Click to Select"){
+      console.log("unaligned")
+      this.readyToMake = false;
+    }
+    if(this.name)
+      console.log(this.readyToMake)
+    console.log(this.readyToMake)
+  }
 
   constructor() { }
 
   ngOnInit(): void {
   }
-  selectClass(i: Item|null){ this.selectedClass=i; }
-  selectBackground(i: Item|null){ this.selectedBackground=i; }
-  selectRace(i: Item|null){ this.selectedRace=i; }
+  selectClass(i: Item|null){ this.selectedClass=i;this.checkReady(); }
+  selectBackground(i: Item|null){ this.selectedBackground=i;this.checkReady(); }
+  selectRace(i: Item|null){ this.selectedRace=i;this.checkReady(); }
   selectTab(tab: string){
     this.selectedTab=tab;
   }
   makeChar(){
     //TODO update DB
+    let char = new StatsService();
+    if(this.name)char.character.permanantStats.characterName=this.name;
+    // char.character.permanantStats.alignmentGoodEvil=this.align.indexOf("Good")>-1?types.GoodEvil.Good:this.align.indexOf("Evil")>-1?types.GoodEvil.Evil:types.GoodEvil.Neutral;
+    // char.character.permanantStats.alignmentGoodEvil=this.align.indexOf("Chaotic")>-1?types.LawfulChaotic.Chaotic:this.align.indexOf("Lawful")>-1?types.LawfulChaotic.Lawful:types.LawfulChaotic.Neutral;
     this.create.emit(this.name);
   }
 
